279bb537614ec502c08a560cc4cd02ad59182eec
Muhammad Hammad <muhammadhammad20004@gmail.com>
2025-09-03 21:46:39 +0800

    Home Page started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
 const App = () => {
   return (
-    <div>
-      
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain ">
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/login' element={ <Login /> } />
+        <Route path='/profile' element={ <Profile /> } />
+      </Routes>
     </div>
   )
 }
